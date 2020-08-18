d75ae34f45f111456b06502abaa4999ee769c863
Beautyxiaoxingxing888 <v_tanpeixin@baidu.com>
2020-08-18 23:45:45 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hihihi</div>
+  <Frank/>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank //映射
+  }
+}
+</script>
